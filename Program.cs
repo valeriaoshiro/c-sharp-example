cd723902fa8b7ee9f332816a1981b24696f2edd6
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-05-23 10:40:39 -0700

    Example of classes. Finished

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Vale", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+			account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+			Console.WriteLine(account.Balance);
+			account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+			Console.WriteLine(account.Balance);
+			Console.WriteLine(account.GetAccountHistory());
+			
+        }
+    }
+}
